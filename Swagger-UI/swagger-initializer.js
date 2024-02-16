7ff9c70364a042ca89c98e720c9b5f2ddf9d4e00
s-crypt <41712656+s-crypt@users.noreply.github.com>
2024-02-15 19:59:25 -0800

    Update swagger-initializer.js

--- Swagger-UI/swagger-initializer.js
+++ Swagger-UI/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./IGDB-OpenAPI.yaml",
+	url: "https://raw.githubusercontent.com/s-crypt/IGDB-OpenAPI/v4/IGDB-OpenAPI.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
 	displayRequestDuration: true,
@@ -16,10 +16,6 @@ window.onload = function() {
       SwaggerUIBundle.plugins.DownloadUrl
     ],
     layout: "BaseLayout",
-	requestInterceptor: function() {
-          this.url = 'https://api.scalar.com/request-proxy' + '/' + this.url // will change the URL used.
-          return this;
-	},
   });
 
   //</editor-fold>
